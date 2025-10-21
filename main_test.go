180fe57f78d9405834fd58ac7266d211cf7b019e
cookel2 <eleanor.cook@ons.gov.uk>
2025-10-21 17:28:06 +0100

    revent unnecessary changes

--- main_test.go
+++ main_test.go
@@ -17,7 +17,7 @@ import (
 var componentFlag = flag.Bool("component", false, "perform component tests")
 
 const mongoVersion = "4.4.8"
-const databaseName = "migrations"
+const databaseName = "testing"
 
 type ComponentTest struct {
 	Mongo *componentTest.MongoFeature
@@ -50,7 +50,6 @@ func (f *ComponentTest) InitializeScenario(godogCtx *godog.ScenarioContext) {
 
 	apiFeature.RegisterSteps(godogCtx)
 	migrationComponent.RegisterSteps(godogCtx)
-	f.Mongo.RegisterSteps(godogCtx)
 }
 
 func (f *ComponentTest) InitializeTestSuite(ctx *godog.TestSuiteContext) {
