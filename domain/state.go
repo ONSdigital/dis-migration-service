9ddafdf5596dd30c27b0f1ceb92e15ee8483c8c1
aryan-patel <aryanpatel1771@gmail.com>
2026-06-02 15:39:21 +0100

    add function to map states to human readable labels

--- domain/state.go
+++ domain/state.go
@@ -1,5 +1,9 @@
 package domain
 
+import (
+	"fmt"
+)
+
 // State represents the various states a migration job can be in.
 type State string
 
@@ -56,7 +60,7 @@ func IsValidState(state State) bool {
 // 'cancelled' state.
 func GetNonCancelledStates() []State {
 	return []State{
-		StateSubmitted, StateInReview, StateApproved, StatePublished,
+		StateSubmitted, StateInReview, StateApproved, StateRejected, StatePublished,
 		StateCompleted, StateMigrating, StatePublishing, StatePostPublishing,
 		StateReverting, StateFailedMigration, StateFailedPostPublish, StateFailedPublish, StateFailedReversion,
 	}
@@ -69,5 +73,43 @@ func IsFailedState(state State) bool {
 		return true
 	default:
 		return false
+	}
+}
+
+// GetStateLabel returns a human readable label for the given State.
+func GetStateLabel(state State) (string, error) {
+	switch state {
+	case StateSubmitted:
+		return "Submitted", nil
+	case StateInReview:
+		return "In review", nil
+	case StateApproved:
+		return "Approved", nil
+	case StateRejected:
+		return "Rejected", nil
+	case StatePublished:
+		return "Published", nil
+	case StateCompleted:
+		return "Completed", nil
+	case StateMigrating:
+		return "Migrating", nil
+	case StatePublishing:
+		return "Publishing", nil
+	case StatePostPublishing:
+		return "Post-publishing", nil
+	case StateReverting:
+		return "Reverting", nil
+	case StateFailedPostPublish:
+		return "Failed post-publish", nil
+	case StateFailedPublish:
+		return "Failed publish", nil
+	case StateFailedMigration:
+		return "Failed migration", nil
+	case StateFailedReversion:
+		return "Failed reversion", nil
+	case StateCancelled:
+		return "Cancelled", nil
+	default:
+		return "", fmt.Errorf("unknown state label mapping for state: %s", state)
 	}
 }
